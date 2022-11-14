474dbd9276320186b400703cf8e2cf5bb6d5aeaa
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-11-14 15:08:19 +0100

    fix: fixed layout breakpoints

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,5 @@
 const Layout = ({ children }) => {
-  return <div className="px-5 md:px-16 lg:px-24 2xl:px-36">{children}</div>;
+  return <div className="px-5 md:px-16 2xl:px-36">{children}</div>;
 };
 
 export default Layout;
